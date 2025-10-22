03d8832ecc84c6dee0ec165e0ea0dfd300157bca
dindaaulia9 <dindaaulyaa9@gmail.com>
2025-10-22 15:44:45 +0700

    declarasi variabel

---
+++ week4.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class week4 {
+    public static void main(String[] args) {
+      int nUmur;
+      Scanner ns = new Scanner(System.in);
+        
+    }
+    
+}
